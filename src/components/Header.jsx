158b72946e87fad0498060551afe6728788bf3ca
Majedul Hasan <hasanmajedul@gmail.com>
2023-05-03 01:59:16 +0600

    prfile dropdown

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,6 +1,6 @@
 import { Link, useNavigate } from "react-router-dom"
 import logo from "../assets/logo.png"
-import { useContext } from "react"
+import { useContext, useState } from "react"
 import { AuthContext } from "../providers/AuthProviders"
 import { Tooltip } from 'react-tooltip'
 import 'react-tooltip/dist/react-tooltip.css'
@@ -8,12 +8,14 @@ import 'react-tooltip/dist/react-tooltip.css'
 const Header = () => {
   const { user, logoutUser, setUser} = useContext(AuthContext);
   const navigate = useNavigate()
+  const  [profileVisible, setProfileVisible] = useState(false)
 
   
 const handleLogout  = () => {
   logoutUser()
   .then(() => {
     setUser(null)
+    setProfileVisible(false)
     navigate('/')
 
   })
@@ -21,44 +23,55 @@ const handleLogout  = () => {
 
   return (
     <div className="navbar w-4/5 mx-auto py-0">
-    <div className="flex-1">
-      <a className="normal-case text-xl text-black ">
-        <img className="block w-28" src={logo} alt="logo" />
-      {/* <Logo color= '#4d4d4d' /> */}
-      </a>
-    </div>
-    
-    <div className="flex-none">
-      <ul className="menu menu-horizontal px-1">
-        <li><Link to='/'>Home</Link></li>
+  <div className="navbar-start">
+    <div className="dropdown">
+      <label tabIndex={0} className="btn btn-ghost lg:hidden">
+        <svg xmlns="http://www.w3.org/2000/svg" className="h-5 w-5" fill="none" viewBox="0 0 24 24" stroke="currentColor"><path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M4 6h16M4 12h8m-8 6h16" /></svg>
+      </label>
+      <ul tabIndex={0} className="menu menu-compact dropdown-content mt-3 p-2 shadow bg-base-100 rounded-box w-52">
+      <li><Link to='/'>Home</Link></li>
         <li><Link to='/news'>News</Link></li>
         <li><Link to='/recipes'>All Recipes</Link></li>
         <li><Link to='/all-chefs'>All Chefs </Link></li>
         <li><Link to='/blogs'>Blogs</Link></li>
         <li><Link to='/about'>About us</Link></li>
         <li><Link to='/Contact'>Contact</Link></li>
-        {
-          user &&
-        <li ><Link to='/profile' >{user?.photoURL ? <img className="w-7" data-tooltip-id="user-avatar" src={user?.photoURL} /> : user?.displayName}</Link> </li>
-        }
-        {
-           user ? <>
-           <button className="bg-blue-400 text-white px-7 mx-1 hover:bg-blue-600 rounded-lg"  onClick={handleLogout} >Logout</button>
-         </>
-        : <>
-        <li> <Link className="bg-orange-400  text-white px-7 mx-1 hover:bg-orange-600" to='/login'>Login</Link> </li>
-        <li> <Link className="bg-blue-400 text-white px-7 mx-1 hover:bg-blue-600" to='/register'>Register</Link> </li>
-        </> 
-        }
-         <Tooltip
-        anchorSelect="#user-avatar"
-        place="bottom"
-        content={user?.displayName}
-      />
-        
       </ul>
     </div>
+    <a className="btn btn-ghost normal-case text-xl">daisyUI</a>
   </div>
+  <div className="navbar-center hidden lg:flex">
+    <ul className="menu menu-horizontal px-1">
+    <li><Link to='/'>Home</Link></li>
+        <li><Link to='/news'>News</Link></li>
+        <li><Link to='/recipes'>All Recipes</Link></li>
+        <li><Link to='/all-chefs'>All Chefs </Link></li>
+        <li><Link to='/blogs'>Blogs</Link></li>
+        <li><Link to='/about'>About us</Link></li>
+        <li><Link to='/Contact'>Contact</Link></li>
+        
+    </ul>
+  </div>
+  <div className="navbar-end relative">
+  {
+          user ? 
+        <button onClick={()=>setProfileVisible(x=>!x)} >{user?.photoURL ? <img className="w-7" data-tooltip-id="user-avatar" data-tooltip-content= {user?.displayName} src={user?.photoURL} /> : user?.displayName}</button> : <>
+         <Link className="bg-orange-400  text-white px-7  py-2 rounded-lg mx-1 hover:bg-orange-600" to='/login'>Login</Link>
+         <Link className="bg-blue-400 text-white px-7 py-2 rounded-lg mx-1 hover:bg-blue-600" to='/register'>Register</Link>
+        </> 
+        }
+         {
+    profileVisible ? <div className="w-48 absolute top-6 bg-slate-200  text-right "> <ul className="menu w-full items-end">
+    <li className="flex justify-between w-full" > <Link to='/profile'  ><span> <img className="w-7" data-tooltip-id="user-avatar" data-tooltip-content= {user?.displayName} src={user?.photoURL} /></span><span> {user?.displayName}</span></Link></li>
+    <li className="w-full text-center">
+    <button className="bg-blue-400 text-white w-full flex justify-center  hover:bg-blue-600 rounded-lg"  onClick={handleLogout} >Logout</button>
+    </li>
+  </ul> </div>: null
+   }
+  </div>
+   <Tooltip id="user-avatar" />
+  
+</div>
   ) 
 }
 
